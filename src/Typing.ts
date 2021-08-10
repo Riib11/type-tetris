8ba72334b2ad5219179637285dc05baf4f06bd22
Henry Blanchette <henry@monkeyface.com>
2021-08-10 12:03:00 -0400

    first impl of typing with new unification approach

--- src/Typing.ts
+++ src/Typing.ts
@@ -1,354 +1,196 @@
-import { HoleId, Term, Type, VariableId, TypeVariableId, typeToString, termToString, variableIdToString } from "./language/Syntax";
+import { concatMaps, LinkedMap, singleMap } from "./data/LinkedMap";
+import { List } from "./data/List";
+import { Term, TermAnn, Type, typeToString, TypeVariableId, typeVariableIdToString, VariableId } from "./language/Syntax";
 
-export type Context = Map<VariableId, Type>;
+export type Context = List<Type>;
+export type Constraints = [Type, Type][];
+export type Substitution = LinkedMap<TypeVariableId, Type>;
 
-export function contextToString(context: Context): string {
-  let s = "";
-  context.forEach((type, id) => s += `(${variableIdToString(id)}: ${typeToString(type)})`);
-  return s;
-}
 
-// Inference
+// TODO: During inferece, get the term variable context at hole (which needs to
+//       have unification substititions applied to it).
 
-export type Inference = {
-  type: Type;
 
-  holeTypes: Map<HoleId, Type>;
-  holeContexts: Map<HoleId, Context>;
-
-  maxVariableId: VariableId;
-  maxTypeVariableId: TypeVariableId;
-  maxHoleId: HoleId;
-}
-
-export function infer(term: Term): Inference {
-  let holeScopes: Map<HoleId, VariableId[]> = new Map();
-  let holeTypes: Map<HoleId, Type> = new Map();
-  let variables: Map<VariableId, Type> = new Map();
-  let typeVariables: Map<TypeVariableId, Type | undefined> = new Map(); // `unidentified` indicates free type variable
-  let visibleVariableIds: VariableId[] = [];
-
-  // Term Variables
-
-  function inferVariable(id: VariableId): Type {
-    let type = variables.get(id);
-    if (type !== undefined) 
-      return type;
-    else
-      throw new Error(`undefined variable: ${id}`);
-  }
-
-  // Type Variables
-
-  function normalizeType(type: Type): Type {
-    switch (type.case) {
-      case "unit": {
-        return type;
-      }
-      case "variable": {
-        if (type.id !== undefined) {
-          let typeNext = typeVariables.get(type.id);
-          if (typeNext !== undefined)
-            return normalizeType(typeNext); // is dependent
-          else
-            return type; // is free
-        } else
-          throw new Error(`unidentified variable`);
-      }
-      case "arrow": {
-        return {case: "arrow", domain: normalizeType(type.domain), codomain: normalizeType(type.codomain)};
-      }
-    }
-  }
-
+export function annotate(term: Term): TermAnn {
+  let freshTypeVariableId = 0;
   function freshTypeVariable(): Type {
-    let maxId = 0;
-    typeVariables.forEach((_, id) => maxId = Math.max(maxId, id))
-    let id = maxId + 1;
-    typeVariables.set(id, undefined);
-    return {case: "variable", id: id};
+    let type: Type = {case: "variable", id: freshTypeVariableId};
+    freshTypeVariableId++;
+    return type;
   }
 
-  // Term Inference
-
-  function inferTerm(term: Term): Type {
+  function go(context: Context, term: Term): TermAnn {
     switch (term.case) {
       case "unit": {
         return {case: "unit"};
       }
       case "variable": {
-        return inferVariable(term.id);
+        let type = inferVariable(context, term.id);
+        return {case: "variable", id: term.id, type};
       }
       case "abstraction": {
-        variables.set(term.id, term.domain);
-        visibleVariableIds.push(term.id);
-        let codomain = inferTerm(term.body);
-        visibleVariableIds.pop();
-        return {case: "arrow", domain: term.domain, codomain};
-      }
-      case "application": {
-        let applicantType = inferTerm(term.applicant);
         let domain: Type = freshTypeVariable();
-        let codomain: Type = freshTypeVariable();
-        unify(applicantType, {case: "arrow", domain, codomain});
-        let argumentType = inferTerm(term.argument);
-        unify(argumentType, domain);
-        return codomain;
+        let body: TermAnn = go({case: "cons", head: domain, tail: context}, term.body);
+        return {case: "abstraction", body, type: {case: "arrow", domain, codomain: extractType(body)}};
       }
       case "hole": {
         let type = freshTypeVariable();
-        holeTypes.set(term.id, type);
-        holeScopes.set(term.id, visibleVariableIds.map(id => id));
-        return type;
+        return {case: "hole", id: term.id, type};
       }
-    }
+    };
+    throw new Error("impossible")
   }
-
-  // Type Unification
-
-  function unify(type1: Type, type2: Type): Type {
-    switch (type1.case) {
-      case "unit": {
-        switch (type2.case) {
-          case "unit": {
-            return {case: "unit"};
-          }
-          case "variable": {
-            typeVariables.set(type2.id, type1);
-            return type1;
-          }
-          case "arrow": {
-            throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)}`);
-          }
-        }
-        break;
-      }
-      case "variable": {
-        switch (type2.case) {
-          case "unit": {
-            typeVariables.set(type1.id, type2);
-            return type2;
-          }
-          case "variable": {
-            typeVariables.set(type1.id, type2);
-            return type2;
-          }
-          case "arrow": {
-            typeVariables.set(type1.id, type2);
-            return type2;
-          }
-        }
-        break;
-      }
-      case "arrow": {
-        switch (type2.case) {
-          case "unit": {
-            throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)}`);
-          }
-          case "variable": {
-            typeVariables.set(type2.id, type1);
-            return type1;
-          } 
-          case "arrow": {
-            unify(type1.domain, type2.domain);
-            unify(type1.codomain, type2.codomain);
-            return type1;
-          }
-        }
-        break;
-      }
-    }
-  }
-
-  // Normalizing Types in Term
-
-  function normalizeTypesInTerm(term: Term): void {
-    switch (term.case) {
-      case "unit": break;
-      case "variable": break;
-      case "abstraction": {
-        term.domain = normalizeType(term.domain);
-        break;
-      }
-      case "application": {
-        normalizeTypesInTerm(term.applicant);
-        normalizeTypesInTerm(term.argument);
-        break;
-      }
-      case "hole": break;
-    }
-  }
-  normalizeTypesInTerm(term);
-
-  let type = normalizeType(inferTerm(term));
-  let holeContexts: Map<HoleId, Context> = new Map();
-  holeScopes.forEach((variableIds, holeId) => {
-    let context: Context = new Map();
-    variableIds.forEach(variableId => context.set(variableId, normalizeType(inferVariable(variableId))));
-    holeContexts.set(holeId, context);
-  })
-  holeTypes.forEach(type => normalizeType(type));
-
-  let maxTypeVariableId = 0;
-  typeVariables.forEach((_, id) => maxTypeVariableId = Math.max(id, maxTypeVariableId));
-
-  let maxVariableId = 0;
-  variables.forEach((_, id) => maxTypeVariableId = Math.max(id, maxVariableId));
-
-  let maxHoleId = 0;
-  holeTypes.forEach((_, id) => maxHoleId = Math.max(id, maxHoleId));
-
-  return {
-    type,
-
-    holeTypes,
-    holeContexts,
-
-    maxVariableId,
-    maxTypeVariableId,
-    maxHoleId
-  };
+  return go({case: "nil"}, term);
 }
 
-export function isSubtype(term: Term, type1: Type, type2: Type): boolean {
-  let variables: Map<VariableId, Type> = new Map();
-  let typeVariables: Map<TypeVariableId, Type | undefined> = new Map(); // `unidentified` indicates free type variable
-
-  // Term Variables
-
-  function inferVariable(id: VariableId): Type {
-    let type = variables.get(id);
-    if (type !== undefined) 
-      return type;
-    else
-      throw new Error(`undefined variable: ${id}`);
-  }
-
-  // Type Variables
-
-  function normalizeType(type: Type): Type {
-    switch (type.case) {
-      case "unit": {
-        return type;
-      }
-      case "variable": {
-        if (type.id !== undefined) {
-          let typeNext = typeVariables.get(type.id);
-          if (typeNext !== undefined)
-            return normalizeType(typeNext); // is dependent
-          else
-            return type; // is free
-        } else
-          throw new Error(`unidentified variable`);
-      }
-      case "arrow": {
-        return {case: "arrow", domain: normalizeType(type.domain), codomain: normalizeType(type.codomain)};
-      }
-    }
-  }
-
-  function freshTypeVariable(): Type {
-    let maxId = 0;
-    typeVariables.forEach((_, id) => maxId = Math.max(maxId, id))
-    let id = maxId + 1;
-    typeVariables.set(id, undefined);
-    return {case: "variable", id: id};
-  }
-
-  // Term Inference
-
-  function inferTerm(term: Term): Type {
+export function collectConstraints(term: TermAnn): Constraints {
+  let constraints: Constraints = [];
+  function go(term: TermAnn): void {
     switch (term.case) {
-      case "unit": {
-        return {case: "unit"};
-      }
-      case "variable": {
-        return inferVariable(term.id);
-      }
-      case "abstraction": {
-        let codomain = inferTerm(term.body);
-        return {case: "arrow", domain: term.domain, codomain};
-      }
+      case "unit": return;
+      case "variable": return;
+      case "abstraction": go(term.body); return;
       case "application": {
-        let applicantType = inferTerm(term.applicant);
-        let domain: Type = freshTypeVariable();
-        let codomain: Type = freshTypeVariable();
-        unify(applicantType, {case: "arrow", domain, codomain});
-        let argumentType = inferTerm(term.argument);
-        unify(argumentType, domain);
-        return codomain;
+        let [f, a] = [extractType(term.applicant), extractType(term.argument)];
+        constraints.push([f, {case: "arrow", domain: a, codomain: term.type}]);
+        go(term.applicant);
+        go(term.argument);
+        return;
       }
-      case "hole": {
-        let type = freshTypeVariable();
+      case "hole": return;
+    }
+  }
+  go(term);
+  return constraints;
+}
+
+export function inferVariable(context: Context, id: VariableId): Type {
+  switch (context.case) {
+    case "nil": throw new Error("variable id out-of-bounds");
+    case "cons": {
+      if (id === 0)
+        return context.head;
+      else
+        return inferVariable(context.tail, id-1);
+    }
+  }
+}
+
+export function infer(term: Term): Type {
+  let termAnn: TermAnn = annotate(term);
+  let constraints: Constraints = collectConstraints(termAnn);
+  let substitution: Substitution = unifyConstraints(constraints);
+  let type = applySubstitution(substitution, extractType(termAnn));
+  return type;
+}
+
+export function substitute(id: TypeVariableId, typeNew: Type, type: Type): Type {
+  switch (type.case) {
+    case "unit": return type;
+    case "variable": {
+      if (type.id === id)
+        return typeNew;
+      else
         return type;
-      }
+    }
+    case "arrow": {
+      return {
+        case: "arrow",
+        domain: substitute(id, typeNew, type.domain),
+        codomain: substitute(id, typeNew, type.codomain)
+      };
     }
   }
+}
 
-  inferTerm(term);
-
-  // Type Unification
-
-  function unify(type1: Type, type2: Type): Type {
-    switch (type1.case) {
-      case "unit": {
-        switch (type2.case) {
-          case "unit": {
-            return {case: "unit"};
-          }
-          case "variable": {
-            typeVariables.set(type2.id, type1);
-            return type1;
-          }
-          case "arrow": {
-            throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)}`);
-          }
-        }
-        break;
-      }
-      case "variable": {
-        switch (type2.case) {
-          case "unit": {
-            typeVariables.set(type1.id, type2);
-            return type2;
-          }
-          case "variable": {
-            typeVariables.set(type1.id, type2);
-            return type2;
-          }
-          case "arrow": {
-            typeVariables.set(type1.id, type2);
-            return type2;
-          }
-        }
-        break;
-      }
-      case "arrow": {
-        switch (type2.case) {
-          case "unit": {
-            throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)}`);
-          }
-          case "variable": {
-            typeVariables.set(type2.id, type1);
-            return type1;
-          } 
-          case "arrow": {
-            unify(type1.domain, type2.domain);
-            unify(type1.codomain, type2.codomain);
-            return type1;
-          }
-        }
-        break;
-      }
+// TODO: this is a fold-right, perhaps it should be a fold-left? Im not sure
+//       which things I got backwards
+export function applySubstitution(substitution: Substitution, type: Type): Type {
+  switch (substitution.case) {
+    case "nil": return type;
+    case "cons": {
+      return substitute(
+        substitution.key, 
+        substitution.value, 
+        applySubstitution(substitution.tail, type));
     }
   }
+}
 
-  type1 = normalizeType(type1);
-  type2 = normalizeType(type2);
-  try {
-    unify(type1, type2);
-    return true;
-  } catch (e) {
-    return false;
+export function unifyConstraints(constraints: Constraints): Substitution {
+  let substitution: Substitution = {case: "nil"};
+  // TODO: this might be applying or concating backwards?
+  constraints.forEach(item => {
+    let sub = unify(applySubstitution(substitution, item[0]), applySubstitution(substitution, item[1]));
+    substitution = concatMaps(substitution, sub);
+  });
+  return substitution;
+}
+
+export function unify(type1: Type, type2: Type): Substitution {
+  switch (type1.case) {
+    case "unit": {
+      switch (type2.case) {
+        case "unit": return {case: "nil"};
+        case "variable": return singleMap(type2.id, type1);
+        case "arrow": throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)}`)
+      }
+      break;
+    }
+    case "variable": {
+      switch (type2.case) {
+        case "unit": return {case: "nil"};
+        case "variable": {
+          if (type1.id === type2.id)
+            return {case: "nil"};
+          else
+            return singleMap(type1.id, type2);
+        }
+        case "arrow": {
+          if (occursIn(type1.id, type2))
+            throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)} because of circularity on ${typeVariableIdToString(type1.id)}`);
+          else
+            return singleMap(type1.id, type2);
+        }
+      }
+      break;
+    }
+    case "arrow": {
+      switch (type2.case) {
+        case "unit": throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)}`)
+        case "variable": {
+          if (occursIn(type2.id, type2))
+            throw new Error(`cannot unify ${typeToString(type1)} with ${typeToString(type2)} because of circularity on ${typeVariableIdToString(type2.id)}`);
+          else
+            return singleMap(type2.id, type1);
+        }
+        case "arrow": {
+          concatMaps(
+            unify(type1.domain, type2.domain),
+            unify(type1.codomain, type2.codomain));
+        }
+      }
+      break;
+    }
+  }
+  throw new Error("unimplemented");
+}
+
+// Checks if `id` occurs in `type`
+export function occursIn(id: TypeVariableId, type: Type): boolean {
+  switch (type.case) {
+    case "unit": return false;
+    case "variable": return id === type.id;
+    case "arrow": return occursIn(id, type.domain) || occursIn(id, type.codomain);
+  }
+}
+
+export function extractType(term: TermAnn): Type {
+  switch (term.case) {
+    case "unit": return {case: "unit"};
+    case "variable": return term.type;
+    case "abstraction": return term.type;
+    case "application": return term.type;
+    case "hole": return term.type;
   }
 }
